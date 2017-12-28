3ad71673b7911404299022c0b0f7d9aaa2bb2995
wendan <dan.wen@360gst.com>
2017-12-28 15:08:16 +0800

    修复webpack 从全局link进来的

--- server/index.js
+++ server/index.js
@@ -11,8 +11,10 @@ const app=require('./app.js').default,
   port = process.env.port || 3002,
   staticCache  = require("koa-static-cache"),
   path =require('path'),
+  cors=require('koa2-cors'),
   Loadable=require('react-loadable');
 
+app.use(cors());
 app.use(clientRouter);
 app.use(staticCache (path.resolve(__dirname,'../dist'),{
   maxAge: 365 * 24 * 60 * 60,
--- src/store/actions/home.js
+++ src/store/actions/home.js
@@ -4,6 +4,7 @@ export const add=(count)=>({type: ADD, count,})
 export const getHomeInfo=()=>async(dispatch,getState)=>{
   let {name,age}=getState().homeInfo;
   if(name || age)return
+  console.log('footer'.includes('foo'))
   await new Promise(resolve=>{
     let homeInfo={name:'wd2010',age:'25'}
     console.log('-----------请求getHomeInfo')
